e18fc789de1b7a7ab685ea0702d5e397f59dfc2c
BruceAko <dczdcz2001@aliyun.com>
2023-08-26 19:35:59 +0800

    finish streamlight

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,22 +1,29 @@
-#include "Vtop.h"
-#include "verilated.h"
+#include <nvboard.h>
+#include <Vlight.h>
 
-int main(int argc, char **argv)
-{
-  VerilatedContext *contextp = new VerilatedContext;
-  contextp->commandArgs(argc, argv);
-  Vtop *top = new Vtop{contextp};
-  while (!contextp->gotFinish())
-  {
-    int a = rand() & 1;
-    int b = rand() & 1;
-    top->a = a;
-    top->b = b;
-    top->eval();
-    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-    assert(top->f == (a ^ b));
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(Vlight* top);
+
+static void single_cycle() {
+  dut.clk = 0; dut.eval();
+  dut.clk = 1; dut.eval();
+}
+
+static void reset(int n) {
+  dut.rst = 1;
+  while (n -- > 0) single_cycle();
+  dut.rst = 0;
+}
+
+int main() {
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+
+  reset(10);
+
+  while(1) {
+    nvboard_update();
+    single_cycle();
   }
-  delete top;
-  delete contextp;
-  return 0;
 }
