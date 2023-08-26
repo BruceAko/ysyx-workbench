2beef68ecc134aadc88163468e477b6ef9981302
BruceAko <dczdcz2001@aliyun.com>
2023-08-26 15:18:37 +0800

    完成数电lab1的在线测试和问题回答

--- homework/digital-circuit-lab/lab1/vsrc/top.v
+++ homework/digital-circuit-lab/lab1/vsrc/top.v
@@ -59,6 +59,26 @@ module mux2bit41(X0,X1,X2,X3,Y,F);
     input  [1:0] X2;
     input  [1:0] X3;
     input  [1:0] Y;
+    output reg [1:0] F;
+    always @ (*)
+    case (Y)
+        0:
+            F=X0;
+        1:
+            F=X1;
+        2:
+            F=X2;
+        3:
+            F=X3;
+    endcase
+endmodule
+
+module mux2bit41b(X0,X1,X2,X3,Y,F);
+    input  [1:0] X0;
+    input  [1:0] X1;
+    input  [1:0] X2;
+    input  [1:0] X3;
+    input  [1:0] Y;
     output [1:0] F;
     MuxKeyWithDefault #(4, 2, 2) i0 (F, Y, 2'b00, {
                                          2'b00, X0,
