c09001b21a1b5e374a21dfba216a192c88a5ac6f
BruceAko <dczdcz2001@aliyun.com>
2023-08-30 19:09:03 +0800

    finish pa1 info r

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -22,6 +22,10 @@ const char* regs[] = {"$0", "ra", "sp",  "gp",  "tp", "t0", "t1", "t2",
                       "a6", "a7", "s2",  "s3",  "s4", "s5", "s6", "s7",
                       "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
 
-void isa_reg_display() {}
+void isa_reg_display() {
+  for (int i = 0; i < 32; i++) {
+    printf("%s\t0x%x\t%u\n", regs[i], gpr(i), gpr(i));
+  }
+}
 
 word_t isa_reg_str2val(const char* s, bool* success) { return 0; }
