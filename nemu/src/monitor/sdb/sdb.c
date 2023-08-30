490405912149da4444c7853705ea890f2b98dbb3
tracer-ysyx <tracer@ysyx.org>
2023-08-30 21:45:18 +0800

    > compile NEMU ysyx_22040000 邓冲之 Linux s172 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:45:18 up 6 days, 48 min,  1 user,  load average: 0.11, 0.04, 0.01

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -56,7 +56,7 @@ static int cmd_x(char* args) {
     };
   }
   uint32_t n;
-  uint32_t addr;
+  int32_t addr;
   if (sscanf(args, "%u%x", &n, &addr) <= 0) {
     printf("miss argument\n");
     return 0;
