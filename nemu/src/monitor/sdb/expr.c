60237ccb138d2b1116dff867aeb10b8237248fd3
tracer-ysyx <tracer@ysyx.org>
2023-09-02 20:28:15 +0800

    > compile NEMU ysyx_22040000 邓冲之 Linux s172 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:28:15 up 8 days, 23:31,  1 user,  load average: 0.15, 0.11, 0.09

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -118,6 +118,9 @@ static bool make_token(char* e) {
 
 word_t expr(char* e, bool* success) {
   if (!make_token(e)) {
+    for (int i = 0; i < ARRLEN(tokens); i++) {
+      printf("type:%d str:%s\n", tokens[i].type, tokens[i].str);
+    }
     *success = false;
     return 0;
   }
