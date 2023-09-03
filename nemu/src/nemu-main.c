ff8f0b0465284df452023f4fb3cf6e68437fa916
tracer-ysyx <tracer@ysyx.org>
2023-09-03 19:37:17 +0800

    > compile NEMU ysyx_22040000 邓冲之 Linux s172 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:37:17 up 9 days, 22:40,  1 user,  load average: 0.05, 0.12, 0.10

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -15,7 +15,7 @@
 
 #include <common.h>
 
-#include "./monitor/sdb/sdb.h"
+#include "monitor/sdb/sdb.h"
 
 void init_monitor(int, char*[]);
 void am_init_monitor();
@@ -30,7 +30,7 @@ int main(int argc, char* argv[]) {
   init_monitor(argc, argv);
 #endif
 
-  FILE* fp = fopen("./tools/gen-expr/build/input", "r");
+  FILE* fp = fopen("tools/gen-expr/build/input", "r");
   assert(fp != NULL);
   word_t res;
   char buf[65536];
