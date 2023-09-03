ac3586868a477aba9fda647026e196d87cb51f69
BruceAko <dczdcz2001@aliyun.com>
2023-09-03 15:41:18 +0800

    format

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -13,29 +13,27 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <assert.h>
 #include <string.h>
+#include <time.h>
 
 // this should be enough
 static char buf[65536] = {};
-static char code_buf[65536 + 128] = {}; // a little larger than `buf`
-static char *code_format =
-"#include <stdio.h>\n"
-"int main() { "
-"  unsigned result = %s; "
-"  printf(\"%%u\", result); "
-"  return 0; "
-"}";
+static char code_buf[65536 + 128] = {};  // a little larger than `buf`
+static char* code_format =
+    "#include <stdio.h>\n"
+    "int main() { "
+    "  unsigned result = %s; "
+    "  printf(\"%%u\", result); "
+    "  return 0; "
+    "}";
 
-static void gen_rand_expr() {
-  buf[0] = '\0';
-}
+static void gen_rand_expr() { buf[0] = '\0'; }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   int seed = time(0);
   srand(seed);
   int loop = 1;
@@ -43,12 +41,12 @@ int main(int argc, char *argv[]) {
     sscanf(argv[1], "%d", &loop);
   }
   int i;
-  for (i = 0; i < loop; i ++) {
+  for (i = 0; i < loop; i++) {
     gen_rand_expr();
 
     sprintf(code_buf, code_format, buf);
 
-    FILE *fp = fopen("/tmp/.code.c", "w");
+    FILE* fp = fopen("/tmp/.code.c", "w");
     assert(fp != NULL);
     fputs(code_buf, fp);
     fclose(fp);
