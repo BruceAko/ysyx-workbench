6ac22b9a4338dbc219c8a419e8778eee42a44932
tracer-ysyx <tracer@ysyx.org>
2023-09-03 21:33:22 +0800

    > compile NEMU ysyx_22040000 邓冲之 Linux s172 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:33:22 up 10 days, 36 min,  1 user,  load average: 0.13, 0.13, 0.10

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -35,19 +35,19 @@ static struct rule {
   const char* regex;
   int token_type;
 } rules[] = {
-    {" +", TK_NOTYPE},        // spaces
-    {"\\+", '+'},             // plus
-    {"-", '-'},               // subtract
-    {"\\*", '*'},             // multiply
-    {"/", '/'},               // divide
-    {"==", TK_EQ},            // equal
-    {"!=", TK_UNEQ},          // unequal
-    {"&&", TK_AND},           // and
-    {"[0-9]+", TK_DECNUM},    // decimal number
-    {"0x[0-9]+", TK_HEXNUM},  // hexadecimal number
-    {"$[a-z0-9]+", TK_REG},   // register name
-    {"\\(", '('},             // open parenthesis
-    {"\\)", ')'},             // close parenthesis
+    {" +", TK_NOTYPE},         // spaces
+    {"\\+", '+'},              // plus
+    {"-", '-'},                // subtract
+    {"\\*", '*'},              // multiply
+    {"/", '/'},                // divide
+    {"==", TK_EQ},             // equal
+    {"!=", TK_UNEQ},           // unequal
+    {"&&", TK_AND},            // and
+    {"[0-9]+", TK_DECNUM},     // decimal number
+    {"0x[0-9]+", TK_HEXNUM},   // hexadecimal number
+    {"\\$[a-z0-9]+", TK_REG},  // register name
+    {"\\(", '('},              // open parenthesis
+    {"\\)", ')'},              // close parenthesis
 };
 
 #define NR_REGEX ARRLEN(rules)
