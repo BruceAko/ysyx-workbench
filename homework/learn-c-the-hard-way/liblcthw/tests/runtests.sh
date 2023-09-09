cf6dc24e83e3361331f890ef9b89a5bfd59d06c0
BruceAko <dczdcz2001@aliyun.com>
2023-09-09 16:52:16 +0800

    fix runtests.sh

--- homework/learn-c-the-hard-way/liblcthw/tests/runtests.sh
+++ homework/learn-c-the-hard-way/liblcthw/tests/runtests.sh
@@ -2,13 +2,17 @@ echo "Running unit tests:"
 
 for i in tests/*_tests
 do
-    $i 2>&1 >> tests/tests.log
-    if [ ! $? ]
+    if test -f $i
     then
-        echo "ERROR in test $i: here's tests/tests.log"
-        echo "------"
-        tail tests/tests.log
-        exit 1
+        if $VALGRIND ./$i 2>> tests/tests.log
+        then
+            echo $i PASS
+        else
+            echo "ERROR in test $i: here's tests/tests.log"
+            echo "------"
+            tail tests/tests.log
+            exit 1
+        fi
     fi
 done
 
