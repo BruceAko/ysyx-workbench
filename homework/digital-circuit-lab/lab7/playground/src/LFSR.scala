8368d1912161c425b1e529c25cf82e2fe3f1dfc6
tracer-ysyx <tracer@ysyx.org>
2023-09-12 18:44:23 +0800

    > compile NEMU ysyx_22040000 邓冲之 Linux s172 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:44:23 up 18 days, 21:47,  1 user,  load average: 0.13, 0.09, 0.03

---
+++ homework/digital-circuit-lab/lab7/playground/src/LFSR.scala
@@ -0,0 +1,38 @@
+import chisel3._
+import chisel3.util.Cat
+
+class LSFR extends Module {
+  val io = IO(new Bundle {
+    val start    = Input(UInt(1.W))
+    val seed     = Input(UInt(8.W))
+    val out      = Output(UInt(8.W))
+    val seg1_out = Output(UInt(7.W))
+    val seg2_out = Output(UInt(7.W))
+  })
+
+  val cnt            = RegInit(0.U(8.W))
+  val start_reg      = RegInit(0.U(1.W))
+  val start_postedge = Wire(UInt(1.W))
+  val xor_bit        = Wire(UInt(1.W))
+
+  start_postedge := io.start & ~start_reg
+  xor_bit        := cnt(4) ^ cnt(3) ^ cnt(2) ^ cnt(0)
+  io.out         := cnt
+
+  when(start_postedge === 1.U) {
+    cnt := io.seed
+  }.elsewhen(start_reg === 1.U) {
+    cnt := Cat(xor_bit, cnt(7, 1))
+  }
+  start_reg := io.start
+
+  val seg1 = Module(new bcd7seg())
+  seg1.io.en  := io.start
+  seg1.io.b   := io.out(7, 4)
+  io.seg1_out := seg1.io.h
+
+  val seg2 = Module(new bcd7seg())
+  seg2.io.en  := io.start
+  seg2.io.b   := io.out(3, 0)
+  io.seg2_out := seg2.io.h
+}
