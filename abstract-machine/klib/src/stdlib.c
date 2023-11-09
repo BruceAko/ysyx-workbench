2af05fa45f6f558e724c06bfa9a3236144359593
tracer-ysyx <tracer@ysyx.org>
2023-11-09 15:56:11 +0800

    > compile NEMU ysyx_22040000 邓冲之 Linux s172 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:56:11 up 76 days, 18:59,  1 user,  load average: 0.15, 0.27, 0.24

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -1,6 +1,6 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
@@ -8,38 +8,40 @@ static unsigned long int next = 1;
 int rand(void) {
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
-  return (unsigned int)(next/65536) % 32768;
+  return (unsigned int)(next / 65536) % 32768;
 }
 
-void srand(unsigned int seed) {
-  next = seed;
-}
+void srand(unsigned int seed) { next = seed; }
 
-int abs(int x) {
-  return (x < 0 ? -x : x);
-}
+int abs(int x) { return (x < 0 ? -x : x); }
 
 int atoi(const char* nptr) {
   int x = 0;
-  while (*nptr == ' ') { nptr ++; }
+  while (*nptr == ' ') {
+    nptr++;
+  }
   while (*nptr >= '0' && *nptr <= '9') {
     x = x * 10 + *nptr - '0';
-    nptr ++;
+    nptr++;
   }
   return x;
 }
 
-void *malloc(size_t size) {
+extern char _heap_start;
+static void* addr = &_heap_start;
+
+void* malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  void* old_addr = addr;
+  addr += size;
+  return old_addr;
 #endif
   return NULL;
 }
 
-void free(void *ptr) {
-}
+void free(void* ptr) {}
 
 #endif
