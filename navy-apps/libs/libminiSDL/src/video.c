4729eebc0913ebc4ea5e7647ec598262f2fe703d
BruceAko <dczdcz2001@aliyun.com>
2023-11-16 21:58:30 +0800

    运行NSlider和开机菜单

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -1,17 +1,114 @@
 #include <NDL.h>
 #include <assert.h>
 #include <sdl-video.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 void SDL_BlitSurface(SDL_Surface* src, SDL_Rect* srcrect, SDL_Surface* dst, SDL_Rect* dstrect) {
+  // printf("dst->format->BitsPerPixe is %d\n",dst->format->BitsPerPixel);
+  // TODO();
+
   assert(dst && src);
   assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
+  // important:!!!uint32_t not int16_t, otherwise overflow, not learn
+  // the definition from SDL_Rect!, width * height is pretty large!!
+  uint32_t s_start_pos = 0;
+  uint32_t s_sf_row_num = src->h, s_sf_col_num = src->w, s_rec_row_num = src->h, s_rec_col_num = src->w;
+  if (srcrect != NULL) {
+    s_start_pos = srcrect->x + srcrect->y * src->w;
+    s_rec_row_num = srcrect->h;
+    s_rec_col_num = srcrect->w;
+  }
+
+  uint32_t d_start_pos = 0;
+  uint32_t d_sf_row_num = dst->h, d_sf_col_num = dst->w;
+  //  d_rec_row_num = dst->h, d_rec_col_num = dst->w;
+  // printf("x,y,w is %d-- %d-- %d",dstrect->x,dstrect->y,dst->w);
+  if (dstrect != NULL) {
+    d_start_pos = dstrect->x + dstrect->y * dst->w;
+    // d_rec_row_num = (dstrect->h > 0)? dstrect->h : d_rec_row_num;
+    // d_rec_col_num = (dstrect->w > 0)? dstrect->w : d_rec_col_num;
+  }
+
+  // printf("dst x,y,w,h %d,%d,%d,%d\n", dstrect->x, dstrect->y, dstrect->w, dstrect->h);
+  // printf("src x,y,w,h %d,%d,%d,%d\n", srcrect->x, srcrect->y, srcrect->w, srcrect->h);
+  // printf("======================");
+  if (dst->format->BitsPerPixel == 32) {
+    for (int row = 0; row < s_rec_row_num; ++row) {
+      // printf("dst pos is  %d\n", d_sf_col_num);
+      // printf("copy %p, size %d to %p, size %d\n",  src->pixels + s_start_pos + row * s_sf_col_num ,s_rec_col_num, dst->pixels + d_start_pos + row * d_sf_col_num, d_rec_col_num);
+      memcpy((uint32_t*)dst->pixels + d_start_pos + row * d_sf_col_num,
+             (uint32_t*)src->pixels + s_start_pos + row * s_sf_col_num, s_rec_col_num * sizeof(uint32_t));
+    }
+  } else if (dst->format->BitsPerPixel == 8) {
+    for (int row = 0; row < s_rec_row_num; ++row) {
+      // printf("copy %p, size %d to %p, size %d\n",  src->pixels + s_start_pos + row * s_sf_col_num ,s_rec_col_num, dst->pixels + d_start_pos + row * d_sf_col_num, d_rec_col_num);
+      memcpy((uint8_t*)dst->pixels + d_start_pos + row * d_sf_col_num,
+             (uint8_t*)src->pixels + s_start_pos + row * s_sf_col_num, s_rec_col_num * sizeof(uint8_t));
+    }
+  } else
+    panic("unsupported pixel bites %d!\n", dst->format->BitsPerPixel);
 }
 
-void SDL_FillRect(SDL_Surface* dst, SDL_Rect* dstrect, uint32_t color) {}
+void SDL_FillRect(SDL_Surface* dst, SDL_Rect* dstrect, uint32_t color) {
+  // panic();
+  // TODO();
 
-void SDL_UpdateRect(SDL_Surface* s, int x, int y, int w, int h) {}
+  int32_t start_pos = 0;
+  uint32_t sf_row_num = dst->h;
+  uint32_t sf_col_num = dst->w;
+  uint32_t rec_row_num = dst->h;
+  uint32_t rec_col_num = dst->w;
+  if (dstrect != NULL) {
+    start_pos = dstrect->x + dstrect->y * dst->w;
+    rec_row_num = dstrect->h;
+    rec_col_num = dstrect->w;
+  }
+
+  if (dst->format->BitsPerPixel == 32) {
+    for (int row = 0; row < rec_row_num; ++row)
+      memset((uint32_t*)dst->pixels + start_pos + row * sf_col_num, color, rec_col_num * sizeof(uint32_t));
+  } else if (dst->format->BitsPerPixel == 8) {
+    for (int row = 0; row < rec_row_num; ++row)
+      memset((uint8_t*)dst->pixels + start_pos + row * sf_col_num, color, rec_col_num * sizeof(uint8_t));
+  } else
+    panic("unsupported pixel bites %d!\n", dst->format->BitsPerPixel);
+  return;
+}
+
+void SDL_UpdateRect(SDL_Surface* s, int x, int y, int w, int h) {
+  // improtant: only w == 0 && h == 0, then set the s size.
+  if (w == 0 && h == 0) {
+    w = s->w;
+    h = s->h;
+  }
+
+  uint32_t len = w * h;
+  uint32_t* buf = malloc(sizeof(uint32_t) * len);
+  uint32_t start_pos = x + y * s->w;
+  uint32_t i = 0;
+  for (size_t row = 0; row < h; ++row) {
+    for (size_t col = 0; col < w; ++col) {
+      uint32_t offset = col + row * s->w;
+      if (s->format->BitsPerPixel == 32) {
+        // important: s->pixels is 8 bit!!! r g b a r g b a...
+        //            also, NDL_DrawRect buf should be a r g b a r g b....
+        s->pixels[start_pos + offset];
+        buf[i++] = s->pixels[start_pos + 4 * offset + 3] << 24 | s->pixels[start_pos + 4 * offset + 2] << 16 |
+                   s->pixels[start_pos + 4 * offset + 1] << 8 | s->pixels[start_pos + 4 * offset];
+      } else if (s->format->BitsPerPixel == 8) {
+        SDL_Color rgba_color = s->format->palette->colors[s->pixels[start_pos + offset]];
+        buf[i++] = rgba_color.a << 24 | rgba_color.r << 16 | rgba_color.g << 8 | rgba_color.b;
+      } else
+        panic("unsupported pixel bites %d!\n", s->format->BitsPerPixel);
+    }
+  }
+
+  NDL_DrawRect(buf, x, y, w, h);
+
+  free(buf);
+}
 
 // APIs below are already implemented.
 
--- navy-apps/libs/libndl/include/NDL.h
+++ navy-apps/libs/libndl/include/NDL.h
@@ -1,6 +1,7 @@
 #ifndef __NDL_H__
 #define __NDL_H__
 
+#include <assert.h>
 #include <stdint.h>
 
 #ifdef __cplusplus
@@ -10,13 +11,21 @@ extern "C" {
 int NDL_Init(uint32_t flags);
 void NDL_Quit();
 uint32_t NDL_GetTicks();
-void NDL_OpenCanvas(int *w, int *h);
-int NDL_PollEvent(char *buf, int len);
-void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h);
+void NDL_OpenCanvas(int* w, int* h);
+int NDL_PollEvent(char* buf, int len);
+void NDL_DrawRect(uint32_t* pixels, int x, int y, int w, int h);
 void NDL_OpenAudio(int freq, int channels, int samples);
 void NDL_CloseAudio();
-int NDL_PlayAudio(void *buf, int len);
+int NDL_PlayAudio(void* buf, int len);
 int NDL_QueryAudio();
+
+#define panic(format, ...)                                                                          \
+  {                                                                                                 \
+    printf("\e[1;35m[%s, %d, %s] \e[0m " format "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
+    assert(0);                                                                                      \
+  }                                                                                                 \
+  while (0)
+#define TODO() panic("PLEASE IMPLEMENT ME!\n")
 
 #ifdef __cplusplus
 }
